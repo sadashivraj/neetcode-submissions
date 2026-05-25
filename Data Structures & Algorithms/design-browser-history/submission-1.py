b0cc1815e1875aba1ddd95383bd79cfa2cc61062
Sadashiv Raj Bharadwaj <kunalb1995@gmail.com>
2026-05-25 14:18:10 +0530

    Add: design-browser-history - submission-1

---
+++ Data Structures & Algorithms/design-browser-history/submission-1.py
@@ -0,0 +1,37 @@
+
+class Node:
+    def __init__(self, page, next=None, prev=None):
+        self.val = page
+        self.next = next
+        self.prev = prev
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.cur = Node(homepage)
+        
+
+    def visit(self, url: str) -> None:
+        self.cur.next = Node(url, None, self.cur)
+        self.cur = self.cur.next
+
+    def back(self, steps: int) -> str:
+        while self.cur.prev and steps>0:
+            self.cur = self.cur.prev
+            steps-=1
+        return self.cur.val
+
+        
+
+    def forward(self, steps: int) -> str:
+        while self.cur.next and steps>0:
+            self.cur = self.cur.next
+            steps-=1
+        return self.cur.val
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
