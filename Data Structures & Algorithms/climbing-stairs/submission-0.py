fe3e576b06ebe24cf4e8210ce0683e6de9574baa
Sadashiv Raj Bharadwaj <kunalb1995@gmail.com>
2026-05-26 15:29:39 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [1,2]
+
+        if n <=2:
+            return dp[n-1]
+        i = 2
+        while i <=n:
+            tmp = dp[1]
+            dp[1] = dp[1] + dp[0]
+            dp[0] = tmp
+            i+=1
+        
+        return dp[0]
+
+
+        
+        
