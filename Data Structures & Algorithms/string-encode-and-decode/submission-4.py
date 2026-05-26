790a737bf4c194b7c687789a5e8561a3174e79ae
Sadashiv Raj Bharadwaj <kunalb1995@gmail.com>
2026-05-26 23:08:52 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ''
+        for sub_str in strs:
+            encoded += str(len(sub_str)) + "^" +  sub_str
+
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != '^':
+                j += 1
+            length = int(s[i:j])
+            res.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        return res
